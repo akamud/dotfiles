7944c1d0b596e208aa6d75999f70e8b470bcc82f
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2019-02-28 11:57:12 -0300

    Update OpenJDK and iTerm2 settings

--- environment_variables.zsh
+++ environment_variables.zsh
@@ -4,3 +4,4 @@ export LANG=en_US.UTF-8
 
 # Android Home
 export ANDROID_HOME=/Users/Mahmoud/Library/Developer/Xamarin/android-sdk-macosx
+export JAVA_HOME=/Users/Mahmoud/Library/Developer/Xamarin/jdk/microsoft_dist_openjdk_1.8.0.9
--- path.zsh
+++ path.zsh
@@ -3,3 +3,6 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
 # Fastlane
 export PATH="$HOME/.fastlane/bin:$PATH"
+
+# Open JDK
+export PATH="$JAVA_HOME/bin:$PATH"
